2c19fea3de4885150d4ad6a03145eaeaa576f228
Carolina Lopes <lincklopescarol@gmail.com>
2026-03-04 21:05:48 -0300

    fix: ajuste para dar like

--- 4869--react-router-code-connect/src/components/CardPost/index.jsx
+++ 4869--react-router-code-connect/src/components/CardPost/index.jsx
@@ -9,8 +9,13 @@ export const CardPost = ({ post }) => {
   const [likes, setLikes] = useState(post.likes);
 
   const handleLikeButton = () => {
+    const token = localStorage.getItem("access_token");
+
     fetch(`http://localhost:3000/blog-posts/${post.id}/like`, {
       method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }).then((response) => {
       if (response.ok) {
         setLikes((oldState) => oldState + 1);
